0fc69a205d01f4bec3dd317a57613ff3b0afde92
Илья <fetisov.ilya.andreevich@gmail.com>
2022-06-10 15:39:55 +0300

    Finished

--- src/ru/skypro/Author.java
+++ src/ru/skypro/Author.java
@@ -1,4 +1,27 @@
 package ru.skypro;
 
 public class Author {
+    private String name;
+    private String surName;
+
+    public Author(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurName() {
+        return this.surName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }
--- src/ru/skypro/Book.java
+++ src/ru/skypro/Book.java
@@ -1,4 +1,37 @@
 package ru.skypro;
 
 public class Book {
+    private String name;
+    private Author author;
+    private int year;
+
+    public Book(String name, Author author, int pages) {
+        this.name = name;
+        this.author = author;
+        this.year = pages;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
